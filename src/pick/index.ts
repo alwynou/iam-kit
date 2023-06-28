2c90396a360c744367ca417aae622bf5cc80f6df
alwynou <alwynou@163.com>
2023-06-28 15:14:43 +0800

    feat: add `pick`

---
+++ src/pick/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Pick part of prop form `obj`
+ *
+ * @param {object} obj
+ * @param {Array<string>} keys
+ */
+export function pick<T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Pick<T, K>
+export function pick<T extends object, K extends keyof T>(
+  obj: T,
+  ...keys: K[]
+): Pick<T, K>
+export function pick<T extends object, K extends keyof T>(
+  obj: T,
+  ...keys: K[]
+) {
+  if (Array.isArray(keys[0])) keys = keys[0]
+
+  if (Object.keys(obj).length === keys.length) return { ...obj }
+
+  const result = {} as Pick<string, any>
+
+  for (const k of keys) {
+    if (k in obj) result[k] = obj[k]
+  }
+
+  return result
+}
+
+export default pick
