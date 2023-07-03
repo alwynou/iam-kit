93083563948cb46806d6b12a081cd387e2bb24e7
alwynou <alwynou@163.com>
2023-07-03 11:26:23 +0800

    feat(get): support `default-value` argument

--- src/get/index.test.ts
+++ src/get/index.test.ts
@@ -51,4 +51,22 @@ describe('get', () => {
     }
     expect(get(obj, 'a.1.b.c')).toBe(456)
   })
+
+  it('should return the default value if the path does not exist', () => {
+    const obj = {
+      a: {
+        b: {
+          c: 456
+        },
+        d: 0,
+        e: false,
+        f: null
+      }
+    }
+    expect(get(obj, 'a.d', 'N/A')).toEqual(0)
+    expect(get(obj, 'a.e', 'N/A')).toEqual(false)
+    expect(get(obj, 'a.f', 'N/A')).toEqual(null)
+    expect(get(obj, 'a.g', 'N/A')).toEqual('N/A')
+    expect(get(obj, 'a.g')).toBeUndefined()
+  })
 })
--- src/get/index.ts
+++ src/get/index.ts
@@ -1,29 +1,31 @@
-import { isObject } from '../isObject'
+import { isObject } from '../isObject/index'
 
 /**
- * Returns the value at the specified path in the given object.
+ * Retrieves the value at a given path from an object.
  *
- * @param {T} obj - The object to retrieve the value from.
- * @param {K} path - The path to the value.
- * @return {Get<T, K>} The value at the specified path.
+ * @param {T} obj - The object from which to retrieve the value.
+ * @param {K} path - The path to the value in dot notation.
+ * @param {D} [defValue] - The default value to return if the path does not exist.
+ * @return {Get<T, K, D>} - The value at the given path or the default value.
  */
-export function get<T extends object, K extends string>(
+export function get<T extends object, K extends string, D = undefined>(
   obj: T,
-  path: K
-): Get<T, K> {
+  path: K,
+  defValue?: D
+): Get<T, K, D> {
   const pathArr = path?.split('.').filter(Boolean) ?? []
   let curValue = obj as unknown as any
   while (pathArr.length > 0 && isObject(curValue)) {
     const curKey = pathArr.shift()!
     curValue = (curValue as any)[curKey]
   }
-  return curValue
+  return curValue === undefined ? defValue : curValue
 }
 
-type Get<T, K> = K extends `${infer L}.${infer R}`
+type Get<T, K, D> = K extends `${infer L}.${infer R}`
   ? L extends keyof T
-    ? Get<T[L], R>
-    : undefined
+    ? Get<T[L], R, D>
+    : D
   : K extends keyof T
   ? T[K]
-  : undefined
+  : D
