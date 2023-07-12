9e8f56bc07f09bb6365d3bce080af5179f5328e7
alwynou <alwynou@163.com>
2023-07-12 13:41:57 +0800

    pref(plimit)!: auto omit not function type arguments

--- src/plimit/index.ts
+++ src/plimit/index.ts
@@ -1,22 +1,23 @@
 import { isFunction } from '../isFunction'
-import psequence, { type SequenceNext } from '../psequence'
+import { pcontrol } from '../pcontrol'
+import type { ControlNext } from '../pcontrol'
 
 /**
  * Executes an array of promise functions or async functions in parallel with a limit on the number of concurrent executions.
  *
  * @param {T[]} promiseFns - An array of promise functions or async functions.
  * @param {PlimitConfig} [config={}] - Configuration options for controlling the concurrency limit.
- * @param {number} [config.limit=1] - The maximum number of promise functions or async functions that can be executed concurrently.
+ * @param {number} [config.limit=5] - The maximum number of promise functions or async functions that can be executed concurrently.
  * @return {Promise<PlimitReturn<T>>} A promise that resolves to an array containing the results of all the executed promise functions or async functions.
  */
 export function plimit<T extends PlimitItem[]>(
   promiseFns: T,
   config: PlimitConfig = {}
 ): PlimitReturn<T> {
-  const { limit = 1 } = config
+  const { limit = 5 } = config
   const result = [] as any
   const wrapSlice = (originFns: PlimitItem[]) => {
-    return async (next: SequenceNext) => {
+    return async (next: ControlNext) => {
       const ret = await Promise.all(
         originFns.map(f => (isFunction(f) ? f() : f))
       )
@@ -36,7 +37,7 @@ export function plimit<T extends PlimitItem[]>(
     }
     return slicedPromiseFns.map(wrapSlice)
   }
-  const runner = psequence(sliceFn(promiseFns))
+  const runner = pcontrol(sliceFn(promiseFns))
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
     try {
@@ -55,7 +56,7 @@ type PlimitItem = Promise<unknown> | ((...args: unknown[]) => Promise<unknown>)
 type PlimitReturn<T extends unknown[]> = Promise<{
   [P in keyof T]: T[P] extends Promise<unknown>
     ? Awaited<T[P]>
-    : T[P] extends (...args: unknown[]) => Promise<infer P>
+    : T[P] extends (...args: any[]) => Promise<infer P>
     ? Awaited<P>
     : any
 }>
