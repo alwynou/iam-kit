f5d4ca19eeec24afce0401c2c08a77c6cfbc67de
alwynou <alwynou@163.com>
2023-07-12 16:09:09 +0800

    chore: add commit linter

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line unicorn/prefer-module
+import fg from 'fast-glob'
+
+function getPackages(packagePath) {
+  return fg.sync('*', { cwd: packagePath, onlyDirectories: true })
+}
+
+// eslint-disable-next-line unicorn/prefer-module
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ],
+
+    'scope-enum': [2, 'always', [...getPackages('src')]],
+
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always']
+  }
+}
