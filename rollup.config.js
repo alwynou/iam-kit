69a985966be5e26534860806630895aca63769ed
alwynou <alwynou@163.com>
2023-07-13 15:01:17 +0800

    chore: replace the `unbuild` bundler with `rollup`

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'rollup'
+import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+
+const input = './src/index.ts'
+const dir = 'lib'
+export default defineConfig([
+  {
+    input,
+    output: [
+      {
+        dir,
+        format: 'esm',
+        preserveModules: true,
+        entryFileNames: '[name].mjs',
+        preserveModulesRoot: 'src',
+        exports: 'named'
+      },
+      {
+        dir,
+        format: 'cjs',
+        preserveModules: true,
+        entryFileNames: '[name].cjs',
+        preserveModulesRoot: 'src',
+        exports: 'named'
+      }
+    ],
+    plugins: [esbuild()]
+  },
+  {
+    input,
+    output: {
+      dir,
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src'
+    },
+    plugins: [dts()]
+  }
+])
---
+++ script/post-build.js
@@ -0,0 +1,14 @@
+import fs from 'node:fs/promises'
+import { URL, fileURLToPath } from 'node:url'
+
+async function bootstrap() {
+  const path = fileURLToPath(new URL('../lib/index.d.ts', import.meta.url))
+
+  const indexTypeContet = await fs.readFile(path, 'utf8')
+
+  const replacedContent = indexTypeContet.replace(/index\.js/g, 'index.d.ts')
+
+  await fs.writeFile(path, replacedContent, 'utf8')
+}
+
+bootstrap()
