73f661f2483e776d0f1ed7003d60481d8f456281
alwynou <alwynou@163.com>
2023-07-14 14:37:15 +0800

    chore: update scripts

--- script/post-build.js
+++ script/post-build.js
@@ -1,14 +1,10 @@
 import fs from 'node:fs/promises'
 import { URL, fileURLToPath } from 'node:url'
 
-async function bootstrap() {
-  const path = fileURLToPath(new URL('../lib/index.d.ts', import.meta.url))
+const path = fileURLToPath(new URL('../lib/index.d.ts', import.meta.url))
 
-  const indexTypeContet = await fs.readFile(path, 'utf8')
+const indexTypeContet = await fs.readFile(path, 'utf8')
 
-  const replacedContent = indexTypeContet.replace(/index\.js/g, 'index')
+const replacedContent = indexTypeContet.replace(/index\.js/g, 'index')
 
-  await fs.writeFile(path, replacedContent, 'utf8')
-}
-
-bootstrap()
+await fs.writeFile(path, replacedContent, 'utf8')
