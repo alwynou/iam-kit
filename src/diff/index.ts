adf3af525a004a6c350fe5ac1abd975553bf4a4b
alwynou <alwynou@gmail.com>
2023-09-27 10:38:13 +0800

    chore: improve TS type on `diff` fn

--- src/diff/index.ts
+++ src/diff/index.ts
@@ -5,39 +5,39 @@ import isObject from '../isObject/index'
 import { typeIs } from '../typeIs/index'
 
 const cycleSymbol = Symbol()
-
-function diffImpl<T extends object, U extends object>(
+type Obj = Record<any, any>
+function diffImpl<T extends Obj, U extends Obj>(
   obj1: T,
   obj2: U,
   cache = new Set()
-): any {
+): DeepPartial<T & U> {
   if ((!isObject(obj1) && !isObject(obj2)) || typeIs(obj1) !== typeIs(obj2))
     return obj2
 
-  if (cache.has(obj2)) return cycleSymbol
+  if (cache.has(obj2)) return cycleSymbol as any
 
-  const result: any = isObject(obj1, true) ? {} : []
+  const result: Record<any, any> = isObject(obj1, true) ? {} : []
 
   cache.add(obj2)
 
-  forEach(obj2 as any, (value: any, key: any) => {
-    if (typeIs(value) !== typeIs((obj1 as any)[key])) {
+  forEach(obj2, (value, key) => {
+    if (typeIs(value) !== typeIs(obj1[key])) {
       result[key] = value
-    } else if (isObject(value) && isObject((obj1 as any)[key])) {
-      const ret = diffImpl((obj1 as any)[key], value, cache)
-      if (ret !== cycleSymbol) result[key] = ret
-    } else if (!Object.is(value, (obj1 as any)[key])) {
+    } else if (isObject(value) && isObject(obj1[key])) {
+      const ret = diffImpl(obj1[key], value, cache)
+      if (ret !== (cycleSymbol as any)) result[key] = ret
+    } else if (!Object.is(value, obj1[key])) {
       result[key] = value
     }
   })
 
-  forEach(obj1 as any, (_value: any, key: any) => {
-    if (!isDefined((obj2 as any)[key])) {
+  forEach(obj1, (_value, key) => {
+    if (!isDefined(obj2[key])) {
       result[key] = undefined
     }
   })
 
-  return isArray(result) ? result.filter(isDefined) : result
+  return (isArray(result) ? result.filter(isDefined) : result) as any
 }
 
 /**
@@ -47,8 +47,15 @@ function diffImpl<T extends object, U extends object>(
  * @param {Object} obj2 - The second object to compare.
  * @return {Object} The difference between obj1 and obj2.
  */
-export function diff<T extends object, U extends object>(obj1: T, obj2: U): U {
+export function diff<T extends object, U extends object>(
+  obj1: T,
+  obj2: U
+): DeepPartial<T & U> {
   return diffImpl(obj1, obj2)
+}
+
+type DeepPartial<T extends object> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
 }
 
 export default diff
